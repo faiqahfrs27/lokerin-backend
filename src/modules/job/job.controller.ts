a32e08ef5dbecc18e68341aa07b8d95cf3950532
nailazzahraputris <nailazzahraputris@gmail.com>
2026-06-04 15:17:46 +0700

    fix(job): resolve companyId from userId in DB instead of JWT payload

--- src/modules/job/job.controller.ts
+++ src/modules/job/job.controller.ts
@@ -1,57 +1,52 @@
 import { Request, Response } from "express";
-import { ApiError } from "../../utils/api-error.js";
 import { JobService } from "./job.service.js";
 import { QueryJobDTO } from "./dto/query-job.dto.js";
 
 export class JobController {
   constructor(private jobService: JobService) {}
 
-  private getCompanyId = (res: Response): string => {
-    const companyId = res.locals.user?.companyId;
-    if (!companyId) {
-      throw new ApiError("Your account is not linked to a company", 403);
-    }
-    return companyId;
+  private getUserId = (res: Response): string => {
+    return res.locals.user?.id;
   };
 
   createJob = async (req: Request, res: Response) => {
-    const companyId = this.getCompanyId(res);
-    const result = await this.jobService.createJob(companyId, req.body);
+    const userId = this.getUserId(res);
+    const result = await this.jobService.createJob(userId, req.body);
     res.status(201).send(result);
   };
 
   getJobs = async (req: Request, res: Response) => {
-    const companyId = this.getCompanyId(res);
+    const userId = this.getUserId(res);
     const query = req.query as unknown as QueryJobDTO;
-    const result = await this.jobService.getJobs(companyId, query);
+    const result = await this.jobService.getJobs(userId, query);
     res.status(200).send(result);
   };
 
   getJobById = async (req: Request, res: Response) => {
-    const companyId = this.getCompanyId(res);
+    const userId = this.getUserId(res);
     const id = req.params.id as string;
-    const result = await this.jobService.getJobById(id, companyId);
+    const result = await this.jobService.getJobById(id, userId);
     res.status(200).send(result);
   };
 
   updateJob = async (req: Request, res: Response) => {
-    const companyId = this.getCompanyId(res);
+    const userId = this.getUserId(res);
     const id = req.params.id as string;
-    const result = await this.jobService.updateJob(id, companyId, req.body);
+    const result = await this.jobService.updateJob(id, userId, req.body);
     res.status(200).send(result);
   };
 
   togglePublish = async (req: Request, res: Response) => {
-    const companyId = this.getCompanyId(res);
+    const userId = this.getUserId(res);
     const id = req.params.id as string;
-    const result = await this.jobService.togglePublish(id, companyId);
+    const result = await this.jobService.togglePublish(id, userId);
     res.status(200).send(result);
   };
 
   deleteJob = async (req: Request, res: Response) => {
-    const companyId = this.getCompanyId(res);
+    const userId = this.getUserId(res);
     const id = req.params.id as string;
-    const result = await this.jobService.deleteJob(id, companyId);
+    const result = await this.jobService.deleteJob(id, userId);
     res.status(200).send(result);
   };
 }
